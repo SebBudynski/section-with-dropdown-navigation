3c3061a0a3bb20169e47937c1f4d0ff0fbd670e4
Sebastian Budyński <Sebbudynski@gmail.com>
2024-02-20 09:54:21 +0100

    fixed problem with server and start adding scss files

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("."));
+app.use(express.static('.'));
 
 app.listen(port, () => {
-  console.log("App listening at http://localhost:${port}");
+  console.log(`App listening at http://localhost:${port}`);
 });
