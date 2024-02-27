962b015106bbff02bf56fe50cab7dffe5a463636
Sebastian Budyński <Sebbudynski@gmail.com>
2024-02-27 14:30:04 +0100

    Implemented all planned styles and funcionalities. Code cleanup remaining

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const features = document.querySelector(".features");
 const dropdownFeatures = document.querySelector(".dropdown-features");
 const company = document.querySelector(".company");
 const dropdownCompany = document.querySelector(".dropdown-company");
+const hamburgerMenu = document.querySelector(".hamburger-menu img");
+const hamburgerMenuClose = document.querySelector(".hamburger-menu-close img");
 
 //switch dropdowns and switching arrows img
 features.addEventListener("click", () => {
@@ -13,4 +15,12 @@ features.addEventListener("click", () => {
 company.addEventListener("click", () => {
   dropdownCompany.classList.toggle("hidden");
   company.classList.toggle("arrow-switch");
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  document.querySelector("nav").style.display = "flex";
+});
+
+hamburgerMenuClose.addEventListener("click", () => {
+  document.querySelector("nav").style.display = "none";
 });
